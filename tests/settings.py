fd746b3ebd995ed01f9e914769631bea9a7bbb0c
Roald Nefs <info@roaldnefs.com>
2019-09-27 13:42:49 +0200

    Update Python and Django versions for Travis
    Signed-off-by: Roald Nefs <info@roaldnefs.com>

--- tests/settings.py
+++ tests/settings.py
@@ -13,11 +13,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'localhost',
-        'PORT': 5432
+        'NAME': 'travis_ci_test',
     }
 }
 
